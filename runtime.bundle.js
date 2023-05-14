1933bf9c3404a02b85774a44a690b57d6393f490
Abdurahim .M <Abdugreat2@gmail.com>
2023-05-14 23:50:22 +0300

    Updates

--- runtime.bundle.js
+++ runtime.bundle.js
@@ -190,7 +190,7 @@
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
 /******/ 		
-/******/ 		var chunkLoadingGlobal = self["webpackChunkmeals"] = self["webpackChunkmeals"] || [];
+/******/ 		var chunkLoadingGlobal = self["webpackChunktv_show"] = self["webpackChunktv_show"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
 /******/ 	})();
